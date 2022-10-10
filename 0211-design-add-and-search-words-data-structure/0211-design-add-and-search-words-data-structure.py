61e9cd794ccf60058353d3475865eca0883eb673
Vedang Barhate <dvedang4u@gmail.com>
2022-10-10 15:05:49 -0500

    Time: 10458 ms (74.42%), Space: 78 MB (41.88%) - LeetHub

---
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
@@ -0,0 +1,56 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.word = False
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        curr = self.root
+        for char in word:
+            if char not in curr.children:
+                curr.children[char] = TrieNode()
+            curr = curr.children[char]
+        curr.word = True
+
+    def search(self, word: str) -> bool:
+        def dfs(j, root):
+            cur = root
+
+            for i in range(j, len(word)):
+                c = word[i]
+                if c == ".":
+                    for child in cur.children.values():
+                        if dfs(i + 1, child):
+                            return True
+                    return False
+                else:
+                    if c not in cur.children:
+                        return False
+                    cur = cur.children[c]
+            return cur.word
+
+        return dfs(0, self.root)
+        
+#         def dfs(index, root):
+#             curr = root
+#             for i in range(index, len(word)):
+#                 if word[i] == ".":
+#                     for child in curr.children.values():
+#                         if dfs(i+1, child):
+#                             return True
+#                     return False
+#                 else:
+#                     if word[i] not in curr.children:
+#                         return False
+#                     curr = curr.children[word[i]]
+#             return curr.word
+                
+#         return dfs(0, self.root)
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
