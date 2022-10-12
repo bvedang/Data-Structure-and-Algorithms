0ec4d2964d4e27c213a9c9646a2100646bc3e475
Vedang Barhate <dvedang4u@gmail.com>
2022-10-12 17:28:03 -0500

    Time: 70 ms (25.96%), Space: 14.1 MB (35.78%) - LeetHub

---
+++ 0078-subsets/0078-subsets.py
@@ -0,0 +1,15 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        n = len(nums)
+        sub = []
+        def subsequence(i, sub):
+            if i >= n:
+                res.append(sub.copy())
+                return
+            sub.append(nums[i])
+            subsequence(i+1, sub)
+            sub.remove(nums[i])
+            subsequence(i+1, sub)
+        subsequence(0,sub)
+        return res
