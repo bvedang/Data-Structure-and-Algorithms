7d681e73d67586df2017fd8f43592692746fbc7c
Vedang Barhate <dvedang4u@gmail.com>
2022-10-13 16:04:44 -0500

    Time: 72 ms (53.54%), Space: 14.1 MB (59.63%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,15 +1,25 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        def helper(i):
-            if i == len(nums):
-                return [[]]
-            res = []
-            per = helper(i+1)
+        per =[[]]
+        for n in nums:
+            resper =[]
             for p in per:
-                for j in range(len(p)+1):
+                for i in range(len(p)+1):
                     pcopy = p.copy()
-                    pcopy.insert(j, nums[i])
-                    res.append(pcopy)
-            return res
+                    pcopy.insert(i,n)
+                    resper.append(pcopy)
+            per = resper
+        return per
+#         def helper(i):
+#             if i == len(nums):
+#                 return [[]]
+#             res = []
+#             per = helper(i+1)
+#             for p in per:
+#                 for j in range(len(p)+1):
+#                     pcopy = p.copy()
+#                     pcopy.insert(j, nums[i])
+#                     res.append(pcopy)
+#             return res
         
-        return helper(0)
+#         return helper(0)
