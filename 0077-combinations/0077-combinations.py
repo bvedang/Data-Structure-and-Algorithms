1f32754e20063093fd2f9997da37c869a4471658
Vedang Barhate <dvedang4u@gmail.com>
2022-10-13 08:50:22 -0500

    Time: 1072 ms (25.26%), Space: 16 MB (51.19%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -8,9 +8,13 @@ class Solution:
                 return
             if i > n:
                 return 
-            comb.append(i)
-            backtrack(i+1, comb)
-            comb.pop()
-            backtrack(i+1, comb)
+            # comb.append(i)
+            # backtrack(i+1, comb)
+            # comb.pop()
+            # backtrack(i+1, comb)
+            for j in range(i, n+1):
+                comb.append(j)
+                backtrack(j+1, comb)
+                comb.pop()
         backtrack(1,comb)
         return res
