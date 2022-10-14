8894bb5058a7a9c82eb792ec7a2f9a45865fc94b
Vedang Barhate <dvedang4u@gmail.com>
2022-10-14 12:52:14 -0500

    Time: 76 ms (84.32%), Space: 14.6 MB (14.05%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.py
@@ -0,0 +1,30 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        if n == 1:
+            return [["Q"]]
+        res =[]
+        cols = set()
+        positive = set()
+        negative = set()
+        board = [["."]*n for i in range(n)]
+        def helper(r):
+            if r >= n:
+                copy = ["".join(row) for row in  board]
+                res.append(copy)
+                return
+            for c in range(n):
+                if c in cols or (r+c) in positive or (r-c) in negative:
+                    continue
+                
+                cols.add(c)
+                positive.add(r+c)
+                negative.add(r-c)
+                board[r][c] = "Q"
+                helper(r+1)
+                cols.remove(c)
+                positive.remove(r+c)
+                negative.remove(r-c)
+                board[r][c] = "."
+        
+        helper(0)
+        return res
