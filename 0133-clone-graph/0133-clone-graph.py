215a0847d9eae4b8d43b3b565c2827edabae0c7b
Vedang Barhate <dvedang4u@gmail.com>
2022-10-16 10:17:29 -0500

    Time: 87 ms (12.46%), Space: 14.4 MB (26.19%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -9,21 +9,36 @@ class Node:
 class Solution:
     def cloneGraph(self, node: 'Node') -> 'Node':
         if not node:
-            return
+            return node
         
         if len(node.neighbors) == 0:
             newNode = Node(node.val)
             return newNode
-        oldtonew ={}
-        def dfs(node):
-            if node in oldtonew:
-                return oldtonew[node]
-            copy = Node(node.val)
-            oldtonew[node] = copy
-            for neighbor in node.neighbors:
-                copy.neighbors.append(dfs(neighbor))
-            return copy
         
-        return dfs(node)
+        oldtonew ={}
+        oldtonew[node] = Node(node.val,[])
+        q = deque()
+        q.append(node)
+        while q:
+            for i in range(len(q)):
+                n = q.popleft()
+    
+                for neighbor in n.neighbors:
+                    if neighbor not in oldtonew:
+                        oldtonew[neighbor] = Node(neighbor.val,[])
+                        q.append(neighbor)
+                    oldtonew[n].neighbors.append(oldtonew[neighbor])
+        return oldtonew[node]
+                
+#         def dfs(node):
+#             if node in oldtonew:
+#                 return oldtonew[node]
+#             copy = Node(node.val)
+#             oldtonew[node] = copy
+#             for neighbor in node.neighbors:
+#                 copy.neighbors.append(dfs(neighbor))
+#             return copy
+        
+#         return dfs(node)
                 
                 
