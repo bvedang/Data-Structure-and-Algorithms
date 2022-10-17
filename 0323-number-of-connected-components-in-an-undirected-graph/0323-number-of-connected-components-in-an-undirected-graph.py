237c55742f26c90912d779e6ea32c20dfe6a2fc4
Vedang Barhate <dvedang4u@gmail.com>
2022-10-17 15:33:19 -0500

    Time: 237 ms (33.80%), Space: 17.2 MB (22.77%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
@@ -1,38 +1,58 @@
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
-        par = {}
-        rank ={}
-        numcomp = [n]
-        def find(n):
-            if n not in par:
-                par[n] = n
-                rank[n] = 0
-            p = par[n]
-            while p != par[p]:
-                par[p] = par[par[p]]
-                p = par[p]
-            return p
-        
-        def union(n1,n2):
-            p1,p2 = find(n1),find(n2)
-            if p1 == p2:
-                return
-            if rank[n1] > rank[n2]:
-                par[p2] = p1
-                rank[p1] += 1
-                numcomp[0] -=1
-            elif rank[n1] < rank[n2]:
-                par[p1] = p2
-                rank[p2] += 1
-                numcomp[0] -=1
-            else:
-                par[p2] = p1
-                rank[p1] += 1
-                numcomp[0] -=1
-            return
-        
-        
+        graph =defaultdict(list)
         for edge in edges:
-            union(edge[0],edge[1])
+            graph[edge[0]].append(edge[1])
+            graph[edge[1]].append(edge[0])
+            
         
-        return numcomp[0]
+        def dfs(node):
+            visited.add(node)
+            for neighbor in graph[node]:
+                if neighbor not in visited:
+                    dfs(neighbor)
+        
+        
+        count = 0
+        visited  =set()
+        for i in range(n):
+            if i not in visited:
+                dfs(i)
+                count += 1
+        return count
+#         par = {}
+#         rank ={}
+#         numcomp = [n]
+#         def find(n):
+#             if n not in par:
+#                 par[n] = n
+#                 rank[n] = 0
+#             p = par[n]
+#             while p != par[p]:
+#                 par[p] = par[par[p]]
+#                 p = par[p]
+#             return p
+        
+#         def union(n1,n2):
+#             p1,p2 = find(n1),find(n2)
+#             if p1 == p2:
+#                 return
+#             if rank[n1] > rank[n2]:
+#                 par[p2] = p1
+#                 rank[p1] += 1
+#                 numcomp[0] -=1
+#             elif rank[n1] < rank[n2]:
+#                 par[p1] = p2
+#                 rank[p2] += 1
+#                 numcomp[0] -=1
+#             else:
+#                 par[p2] = p1
+#                 rank[p1] += 1
+#                 numcomp[0] -=1
+#             return
+        
+        
+#         for edge in edges:
+#             union(edge[0],edge[1])
+        
+#         return numcomp[0]
