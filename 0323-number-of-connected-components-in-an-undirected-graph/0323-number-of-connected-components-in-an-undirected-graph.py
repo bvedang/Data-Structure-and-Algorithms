7711134350cd02d15c4e2aaf8850a2d7da38b958
Vedang Barhate <dvedang4u@gmail.com>
2022-10-17 15:34:39 -0500

    Time: 253 ms (23.90%), Space: 15.7 MB (47.54%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
@@ -1,24 +1,5 @@
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
-        #DFS solution
-#         graph =defaultdict(list)
-#         for edge in edges:
-#             graph[edge[0]].append(edge[1])
-#             graph[edge[1]].append(edge[0])
-        
-#         def dfs(node):
-#             visited.add(node)
-#             for neighbor in graph[node]:
-#                 if neighbor not in visited:
-#                     dfs(neighbor)
-#         count = 0
-#         visited  =set()
-#         for i in range(n):
-#             if i not in visited:
-#                 dfs(i)
-#                 count += 1
-#         return count
-        ## Union Find Solution
         par = {}
         rank ={}
         numcomp = [n]
@@ -40,9 +21,13 @@ class Solution:
                 par[p2] = p1
                 rank[p1] += 1
                 numcomp[0] -=1
-            else:
+            elif rank[n1] < rank[n2]:
                 par[p1] = p2
                 rank[p2] += 1
+                numcomp[0] -=1
+            else:
+                par[p2] = p1
+                rank[p1] += 1
                 numcomp[0] -=1
             return
         
