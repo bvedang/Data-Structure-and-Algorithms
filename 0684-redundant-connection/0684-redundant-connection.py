241a2973fda85719d7363574c9715ac06c264f49
Vedang Barhate <dvedang4u@gmail.com>
2022-10-17 12:13:19 -0500

    Time: 123 ms (33.74%), Space: 14.5 MB (55.98%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -18,12 +18,15 @@ class Solution:
             p2 = find(n2)
             if p1 == p2:
                 return False
-            if rank[n1] > rank[n2]:
+            if rank[p1] > rank[p2]:
                 par[p2] = p1
                 rank[p1] += 1
-            else:
+            elif rank[p1] < rank[p2]:
                 par[p1] = p2
                 rank[p2] +=1
+            else:
+                par[p2] = p1
+                rank[p1] +=1
             return True
         
         for edge in edges:
