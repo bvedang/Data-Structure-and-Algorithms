22ff81ad0748f6e28c9dd6ee70c1111ea7e05a53
Vedang Barhate <dvedang4u@gmail.com>
2022-10-19 08:45:29 -0500

    Time: 316 ms (57.89%), Space: 18.5 MB (18.59%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.py
@@ -0,0 +1,32 @@
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        if endWord not in wordList:
+            return 0
+            
+        wordList.append(beginWord)
+        graph = defaultdict(list)
+        for word in wordList:
+            for j in range(len(word)):
+                patter = word[:j] + "*" + word[j+1:]                            ## getting all patter hit -> h*t, *ot, ho* and all its adjacent words
+                graph[patter].append(word)
+        
+        print(graph)
+        
+        visited = set()
+        visited.add(beginWord)
+        q = deque([beginWord])
+        res = 1
+        while q:
+            for i in range(len(q)):
+                node = q.popleft()
+                if node == endWord:
+                    return res
+                for j in range(len(node)):
+                    patter = node[:j] + "*" + node[j+1:]
+                    for neighbor in graph[patter]:
+                        if neighbor not in visited:
+                            visited.add(neighbor)
+                            q.append(neighbor)
+            res += 1
+            
+        return 0
