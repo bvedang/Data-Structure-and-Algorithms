3cebd10b48db69a1109e06d44f14d93ca19eaf2b
Vedang Barhate <dvedang4u@gmail.com>
2022-10-20 10:36:40 -0500

    Time: 354 ms (17.73%), Space: 14.7 MB (24.48%) - LeetHub

---
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.py
@@ -0,0 +1,29 @@
+class Solution:
+    def swimInWater(self, grid: List[List[int]]) -> int:
+        if len(grid) == 0:
+            return 0
+        n = len(grid)
+        visited = set()
+        minheap = [(grid[0][0],0,0)]
+        mincost = 0
+        directions = [(1,0),(-1,0),(0,1),(0,-1)]
+        while minheap:
+            w1, si, sj = heapq.heappop(minheap)
+            mincost = max(mincost,w1)
+            if si == n-1 and sj == n-1:
+                break
+            if (si,sj) in visited:
+                continue
+            visited.add((si,sj))
+            for dr,dc in directions:
+                newsr = si + dr
+                newdc = sj + dc
+                if newsr < 0 or newsr == n or newdc < 0 or newdc == n or (newsr,newdc) in visited:
+                    continue # we dont want to get index out of bound
+                if w1 >= grid[newsr][newdc]:
+                    heapq.heappush(minheap,(w1, newsr,newdc))
+                    
+                else:
+                    heapq.heappush(minheap, (grid[newsr][newdc], newsr,newdc))
+                
+        return mincost
