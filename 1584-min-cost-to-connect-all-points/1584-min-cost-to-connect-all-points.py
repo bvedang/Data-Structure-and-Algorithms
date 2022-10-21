890a612909e9f163ff9fd8a1fa8201654b4b56a6
Vedang Barhate <dvedang4u@gmail.com>
2022-10-21 06:34:53 -0500

    Time: 4531 ms (43.63%), Space: 150.9 MB (32.52%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -1,24 +1,26 @@
 class Solution:
-    def minCostConnectPoints(self, points: List[List[int]]) -> int: # time O(N^2 Log N) Space N^2
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        n = len(points)
         adj = defaultdict(list)
         for i in range(len(points)):
-            x1, y1 = points[i]
+            xi,yi = points[i]
             for j in range(i+1, len(points)):
-                x2,y2 = points[j]
-                dist = abs(x1-x2) + abs(y1-y2)
-                adj[i].append((dist,j))
-                adj[j].append((dist,i))
-        visited = set()
+                xj,yj = points[j]
+                dist = abs(xi-xj) + abs(yi-yj)
+                adj[i].append((dist, j))
+                adj[j].append((dist, i))
         minheap = [(0,0)]
+        visited = set()
         cost = 0
-        while len(visited) < len(points):
-            c,i = heapq.heappop(minheap)
-            if i in visited:
+        while len(visited) < n:
+            cst, src = heapq.heappop(minheap)
+            if src in visited:
                 continue
-            cost += c
-            visited.add(i)
-            for dist, j in adj[i]:
-                if j not in visited:
-                    heapq.heappush(minheap, (dist,j))
+            cost += cst
+            visited.add(src)
+            for dist, sr in adj[src]:
+                if sr in visited:
+                    continue
+                heapq.heappush(minheap, (dist, sr))
         
         return cost
