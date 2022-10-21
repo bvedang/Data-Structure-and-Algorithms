b4017ccb3c1be17e07ba6d0aa13e6369e5dc804b
Vedang Barhate <dvedang4u@gmail.com>
2022-10-21 09:51:33 -0500

    Time: 51 ms (57.27%), Space: 13.8 MB (96.03%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,11 +1,21 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        cache = {}
-        def helper(n, cache):
-            if n <=1:
-                return 1
-            if n in cache:
-                return cache[n]
-            cache[n] = helper(n-1,cache) + helper(n-2, cache)
-            return cache[n]
-        return helper(n, cache)
+        #Top down Approcah Time O(N) space O(N) cache
+        if n <2:
+            return 1
+        res = [1,1]
+        i = 2
+        while i <= n:
+            res.append(res[i-1] + res[i-2])
+            i +=1
+        return res[n]
+        # Memoization Method  Time  O(N)  Space O(2*N) stack space, cache
+        # cache = {}
+        # def helper(n, cache):
+        #     if n <=1:
+        #         return 1
+        #     if n in cache:
+        #         return cache[n]
+        #     cache[n] = helper(n-1,cache) + helper(n-2, cache)
+        #     return cache[n]
+        # return helper(n, cache)
