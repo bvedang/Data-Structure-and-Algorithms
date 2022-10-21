cf295e72e077d993b78ee55bc1956cac0ce0eee7
Vedang Barhate <dvedang4u@gmail.com>
2022-10-21 12:28:39 -0500

    Time: 148 ms (11.03%), Space: 13.9 MB (97.41%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,13 +1,27 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
+        
         if len(cost) <= 2:
             return min(cost[1], cost[0])
-        cache={0:0, 1: 0}
+        step1 = step2 = 0
         i = 2
         while i <= len(cost):
-            cache[i] = min((cost[i-1] + cache[i-1]), (cost[i-2] + cache[i-2]))
+            currcost = min((cost[i-1] + step1), (cost[i-2] + step2))
+            step2 = step1
+            step1 = currcost
             i += 1
-        return cache[len(cost)]
+        return step1
+        
+        
+        # Top bottom Time complexity O(N) space O(N)
+        # if len(cost) <= 2:
+        #     return min(cost[1], cost[0])
+        # cache={0:0, 1: 0}
+        # i = 2
+        # while i <= len(cost):
+        #     cache[i] = min((cost[i-1] + cache[i-1]), (cost[i-2] + cache[i-2]))
+        #     i += 1
+        # return cache[len(cost)]
                 
         
         # Memoization Time complexity O(N) space O(N*2)
