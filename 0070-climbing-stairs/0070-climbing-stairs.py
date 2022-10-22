723165df8167d50d479567ac2f3a51b38bb1a0f5
Vedang Barhate <dvedang4u@gmail.com>
2022-10-22 06:18:36 -0500

    Time: 62 ms (20.24%), Space: 14 MB (11.90%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,11 +1,20 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        cache = {0:1,1:1}
+        prev1 =1 
+        prev2 = 1
         if n < 2:
-            return 1
+            return prev1
         for i in range(2,n+1):
-            cache[i] = cache[i-1] + cache[i-2]
-        return cache[n]
+            currcom = prev1 + prev2
+            prev2 = prev1
+            prev1 = currcom
+        return prev1
+        # cache = {0:1,1:1}
+        # if n < 2:
+        #     return 1
+        # for i in range(2,n+1):
+        #     cache[i] = cache[i-1] + cache[i-2]
+        # return cache[n]
         # def helper(n):
         #     if n < 2:
         #         return 1
