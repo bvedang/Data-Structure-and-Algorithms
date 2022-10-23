523b3c7218087b53ad8a446d98e3ffa9eb3661b7
Vedang Barhate <dvedang4u@gmail.com>
2022-10-23 10:14:36 -0500

    Time: 56 ms (53.21%), Space: 13.9 MB (32.01%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,13 +1,36 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
+        ## Top down Time O(M*N) space O(M*N)
         dp = [[0]*n for i in range(m)]
-        def helper(r,c):
-            if r < 0 or c < 0:
-                return 0
-            if r == 0 and c == 0 :
-                return 1
-            if dp[r][c] != 0:
-                return dp[r][c]
-            dp[r][c] = helper(r-1,c) + helper(r,c-1)
-            return dp[r][c]
-        return helper(m-1,n-1)
+        for r in range(m):
+            dp[r][0] = 1
+        for c in range(n):
+            dp[0][c] =1
+        
+        for r in range(1,m):
+            for c in range(1,n):
+                dp[r][c] = dp[r-1][c] + dp[r][c-1]
+        
+        return dp[m-1][n-1]
+        ## Recursion Solution with Memoization Time O(M*N) space O(M*N) +O((M-1)+(N-1))
+#         dp = [[0]*n for i in range(m)]
+#         def helper(r,c):
+#             if r < 0 or c < 0:
+#                 return 0
+#             if r == 0 and c == 0 :
+#                 return 1
+#             if dp[r][c] != 0:
+#                 return dp[r][c]
+            
+#             dp[r][c] = helper(r-1,c) + helper(r,c-1)
+#             return dp[r][c]
+#         return helper(m-1,n-1)
+        ## Recursion Solution Time O(2^M*N) space O((M-1)+(N-1))
+        # def helper(r,c):
+        #     if r < 0 or c < 0:
+        #         return 0
+        #     if r == 0 and c == 0 :
+        #         return 1
+        #     count = helper(r-1,c) + helper(r,c-1)
+        #     return count
+        # return helper(m-1,n-1)
