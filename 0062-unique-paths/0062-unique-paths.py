be5dd134b5b69b299fcc4481591c0f01283de403
Vedang Barhate <dvedang4u@gmail.com>
2022-10-23 10:36:59 -0500

    Time: 63 ms (31.73%), Space: 13.8 MB (74.27%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,17 +1,27 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        ## Top down Time O(M*N) space O(M*N)
-        dp = [[0]*n for i in range(m)]
-        for r in range(m):
-            dp[r][0] = 1
-        for c in range(n):
-            dp[0][c] =1
-        
+        row = [1]*n
         for r in range(1,m):
-            for c in range(1,n):
-                dp[r][c] = dp[r-1][c] + dp[r][c-1]
+            temp = [0]*n
+            for c in range(0,n):
+                if c == 0:
+                    temp[c] = row[c]
+                else:    
+                    temp[c] = row[c] + temp[c-1]
+            row = temp 
+        return row[n-1]
+        ## Top down Time O(M*N) space O(M*N)
+#         dp = [[0]*n for i in range(m)]
+#         for r in range(m):
+#             dp[r][0] = 1
+#         for c in range(n):
+#             dp[0][c] =1
         
-        return dp[m-1][n-1]
+#         for r in range(1,m):
+#             for c in range(1,n):
+#                 dp[r][c] = dp[r-1][c] + dp[r][c-1]
+        
+#         return dp[m-1][n-1]
         ## Recursion Solution with Memoization Time O(M*N) space O(M*N) +O((M-1)+(N-1))
 #         dp = [[0]*n for i in range(m)]
 #         def helper(r,c):
