0872f02c52236554cc3aa5d34793a0509d7371d8
Vedang Barhate <dvedang4u@gmail.com>
2022-10-26 08:29:44 -0500

    Time: 220 ms (47.79%), Space: 15.7 MB (84.05%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -2,21 +2,37 @@ class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
         n = len(grid[0])
         m = len(grid)
+        if m == 1 and n == 1:
+            return grid[0][0]
         cache = [[0]*n for i in range(m)]
+        for r in range(m):
+            if r == 0:
+                cache[r][0] = grid[r][0]
+            cache[r][0] = grid[r][0] + cache[r-1][0]
+        for c in range(n):
+            if c == 0:
+                cache[0][c] = grid[0][c]
+            cache[0][c] = grid[0][c] + cache[0][c-1]
         
-        def helper(r,c):
-            if r<0 or c<0:
-                return float("inf")
-            if r == 0 and c == 0:
-                return grid[0][0]
-            if cache[r][c] != 0:
-                return cache[r][c]
+        for r in range(1,m):
+            for c in range(1,n):
+                cache[r][c] = min(grid[r][c]+cache[r-1][c],grid[r][c]+cache[r][c-1])
+        
+        return cache[m-1][n-1]
+#       Recursion Solution with memoization Time O(M*N) and space O(M*N) + On(m+n)
+#         def helper(r,c):
+#             if r<0 or c<0:
+#                 return float("inf")
+#             if r == 0 and c == 0:
+#                 return grid[0][0]
+#             if cache[r][c] != 0:
+#                 return cache[r][c]
             
-            left = grid[r][c]+ helper(r-1,c)
-            top = grid[r][c] + helper(r,c-1)
-            cache[r][c] = min(left,top)
-            return cache[r][c]
-        return helper(len(grid)-1, len(grid[0])-1)
+#             left = grid[r][c]+ helper(r-1,c)
+#             top = grid[r][c] + helper(r,c-1)
+#             cache[r][c] = min(left,top)
+#             return cache[r][c]
+#         return helper(len(grid)-1, len(grid[0])-1)
 #        Recursion Solution Time O(2^M*N) and space O(2^M*N)
 #         def helper(r,c):
 #             if r<0 or c<0:
