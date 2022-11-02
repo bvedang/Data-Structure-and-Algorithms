e057008f2dd9a77c188c7ed1d3fed7d8b63ce20c
Vedang Barhate <dvedang4u@gmail.com>
2022-11-02 08:50:35 -0500

    Time: 458 ms (10.74%), Space: 16.6 MB (15.54%) - LeetHub

---
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
@@ -0,0 +1,37 @@
+class Solution:
+    def minFallingPathSum(self, matrix: List[List[int]]) -> int:
+        n = len(matrix)
+        cache = [[0]*n for i in range(n)]
+        def helper(r,c):
+            if c<0 or c >= n:
+                return float("inf")
+            if r == n-1 and c < n:
+                return matrix[r][c]
+            if cache[r][c] !=0:
+                return cache[r][c]
+            # down = matrix[r][c] + helper(r+1,c)
+            # left = matrix[r][c] + helper(r+1,c-1)
+            # right = matrix[r][c] + helper(r+1,c+1)
+            cache[r][c] = matrix[r][c] + min(helper(r+1,c),helper(r+1,c-1),helper(r+1,c+1))
+            return cache[r][c]
+        res = helper(0,0)
+        for i in range(1,n):
+            res = min(res, helper(0,i))
+        
+        return res
+#         Recursion Solution Time O(3^N) space(3N)
+#         def helper(r,c):
+#             if c<0 or c >= n:
+#                 return float("inf")
+#             if r == n-1:
+#                 return matrix[r][c]
+            
+#             # down = matrix[r][c] + helper(r+1,c)
+#             # left = matrix[r][c] + helper(r+1,c-1)
+#             # right = matrix[r][c] + helper(r+1,c+1)
+#             return matrix[r][c] + min(helper(r+1,c),helper(r+1,c-1),helper(r+1,c+1))
+#         res = helper(0,0)
+#         for i in range(1,n):
+#             res = min(res, helper(0,i))
+        
+#         return res
