dd834e4cf923e49baf9abd33e2c0538e4d60a7bc
Vedang Barhate <dvedang4u@gmail.com>
2022-11-05 06:20:22 -0500

    Added solution - LeetHub

---
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.py
@@ -0,0 +1,42 @@
+#User function Template for python3
+
+class Solution:
+    
+    #Function to return max value that can be put in knapsack of capacity W.
+    def knapSack(self,W, wt, val, n):
+        cache = [[-1]*(W+1) for i in range(n)]
+        def helper(i, capacity):
+            if i == 0:
+                if (capacity >= wt[0]):
+                    return val[0]
+                return 0
+            if cache[i][capacity] != -1:
+                return cache[i][capacity]
+            notpick = helper(i-1,capacity)
+            pick = float("-inf")
+            if capacity >= wt[i]:
+                pick = val[i]+helper(i-1, capacity-wt[i])
+            cache[i][capacity] = max(pick,notpick)
+            return cache[i][capacity]
+        return helper(n-1, W)
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+import atexit
+import io
+import sys
+
+# Contributed by : Nagendra Jha
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases):
+        n = int(input())
+        W = int(input())
+        val = list(map(int,input().strip().split()))
+        wt = list(map(int,input().strip().split()))
+        ob=Solution()
+        print(ob.knapSack(W,wt,val,n))
+# } Driver Code Ends
