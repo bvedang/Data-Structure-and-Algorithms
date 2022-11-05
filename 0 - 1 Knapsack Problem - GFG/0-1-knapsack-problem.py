750e831b219cafdc0f50e2609df372620062c919
Vedang Barhate <dvedang4u@gmail.com>
2022-11-05 06:45:12 -0500

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.py
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.py
@@ -4,21 +4,31 @@ class Solution:
     
     #Function to return max value that can be put in knapsack of capacity W.
     def knapSack(self,W, wt, val, n):
-        cache = [[-1]*(W+1) for i in range(n)]
-        def helper(i, capacity):
-            if i == 0:
-                if (capacity >= wt[0]):
-                    return val[0]
-                return 0
-            if cache[i][capacity] != -1:
-                return cache[i][capacity]
-            notpick = helper(i-1,capacity)
-            pick = float("-inf")
-            if capacity >= wt[i]:
-                pick = val[i]+helper(i-1, capacity-wt[i])
-            cache[i][capacity] = max(pick,notpick)
-            return cache[i][capacity]
-        return helper(n-1, W)
+        cache = [[0]*(W+1) for i in range(n)]
+        for i in range(wt[0],W+1):
+                cache[0][i] = val[0]
+        for i in range(1,n):
+            for capacity in range(W+1):
+                notpick = cache[i-1][capacity]
+                pick = float("-inf")
+                if capacity >= wt[i]:
+                    pick = val[i] + cache[i-1][capacity-wt[i]]
+                cache[i][capacity] = max(pick,notpick)
+        return cache[n-1][W]
+        # def helper(i, capacity):
+        #     if i == 0:
+        #         if (capacity >= wt[0]):
+        #             return val[0]
+        #         return 0
+        #     if cache[i][capacity] != -1:
+        #         return cache[i][capacity]
+        #     notpick = helper(i-1,capacity)
+        #     pick = float("-inf")
+        #     if capacity >= wt[i]:
+        #         pick = val[i]+helper(i-1, capacity-wt[i])
+        #     cache[i][capacity] = max(pick,notpick)
+        #     return cache[i][capacity]
+        # return helper(n-1, W)
 
 
 #{ 
