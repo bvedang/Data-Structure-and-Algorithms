7bf8856971548059753bac5f11fc0dbeb916ea71
Vedang Barhate <dvedang4u@gmail.com>
2022-11-06 07:59:04 -0600

    Added solution - LeetHub

--- Rod Cutting - GFG/rod-cutting.py
+++ Rod Cutting - GFG/rod-cutting.py
@@ -3,17 +3,29 @@
 class Solution:
     def cutRod(self, price, n):
         dp ={}
-        def helper(ind, N):
-            if ind == 0:  return N*price[0]
-            if (ind,N) in dp: return dp[(ind,N)]
-            nottake = 0 + helper(ind-1,N)
-            take = float("-inf")
-            rodlength = ind+1
-            if rodlength <= N:
-                take = price[ind]+helper(ind,N-rodlength)
-            dp[(ind,N)] = max(take,nottake)
-            return dp[(ind,N)]
-        return helper(n-1, n)
+        for rodlen in range(n+1):
+            dp[(0,rodlen)] = rodlen*price[0]
+        
+        for ind in range(1,len(price)):
+            for rodlen in range(n+1):
+                nottake = 0 + dp[(ind-1, rodlen)]
+                take = float("-inf")
+                temp = ind+1
+                if temp<=rodlen:
+                    take = price[ind]+ dp[(ind,rodlen-temp)]
+                dp[(ind,rodlen)] = max(take, nottake)
+        return dp[(len(price)-1,n)]
+        # def helper(ind, N):
+        #     if ind == 0:  return N*price[0]
+        #     if (ind,N) in dp: return dp[(ind,N)]
+        #     nottake = 0 + helper(ind-1,N)
+        #     take = float("-inf")
+        #     rodlength = ind+1
+        #     if rodlength <= N:
+        #         take = price[ind]+helper(ind,N-rodlength)
+        #     dp[(ind,N)] = max(take,nottake)
+        #     return dp[(ind,N)]
+        # return helper(n-1, n)
 
 
 #{ 
