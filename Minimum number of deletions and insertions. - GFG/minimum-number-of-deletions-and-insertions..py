a061db6b00a546fed873d16de18484024a426efe
Vedang Barhate <dvedang4u@gmail.com>
2022-11-07 08:49:58 -0600

    Added solution - LeetHub

--- Minimum number of deletions and insertions. - GFG/minimum-number-of-deletions-and-insertions..py
+++ Minimum number of deletions and insertions. - GFG/minimum-number-of-deletions-and-insertions..py
@@ -4,15 +4,16 @@ class Solution:
 		def helper(s1,s2):
 		    dp ={}
 		    res = 0
-		    for ind1 in range(len(s1)+1):dp[(ind1,0)] = 0
-		    for ind2 in range(len(s2)+1):dp[(0,ind2)] = 0
+		    for ind2 in range(len(s2)+1):dp[ind2] = 0
 		    for ind1 in range(1,len(s1)+1):
+		        temp = {ind2:0 for ind2 in range(len(s2)+1)}
 		        for ind2 in range(1,len(s2)+1):
 		            if s1[ind1-1] == s2[ind2-1]:
-		                dp[(ind1,ind2)] = 1 + dp[(ind1-1,ind2-1)]
+		                temp[ind2] = 1 + dp[ind2-1]
 		            else:
-		                dp[(ind1,ind2)] = 0+max(dp[(ind1-1,ind2)],dp[(ind1,ind2-1)])
-		    return dp[(len(s1),len(s2))]
+		                temp[ind2] = 0+max(dp[ind2],temp[ind2-1])
+		        dp = temp
+		    return dp[len(s2)]
 		ans = helper(s1,s2)
 		return (len(s1) - ans) + (len(s2)-ans)
 
