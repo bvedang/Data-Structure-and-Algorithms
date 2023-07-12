01bb12e0d62bb409cb8fa45018208676c6bd0bdc
Vedang Barhate <dvedang4u@gmail.com>
2023-07-11 19:57:06 -0500

    Time: 77 ms (5.81%), Space: 42.3 MB (9.59%) - LeetHub

---
+++ 2620-counter/2620-counter.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    let value = n-1
+    return function() {
+        value = value+1
+        return value
+        
+    };
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
